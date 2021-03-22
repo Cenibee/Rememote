e950b8f703834182fe537f5378586fd86ebd04f8
cenibee <ksj3452@naver.com>
2021-03-22 19:59:15 +0900

    :tada:Initialize the project

---
+++ src/main/java/cenibee/github/rememote/RememoteApplication.java
@@ -0,0 +1,13 @@
+package cenibee.github.rememote;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RememoteApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RememoteApplication.class, args);
+    }
+
+}
---
+++ src/test/java/cenibee/github/rememote/RememoteApplicationTests.java
@@ -0,0 +1,13 @@
+package cenibee.github.rememote;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RememoteApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
