4c31e57a9ad54f29b803cafdabad717d895b9310
cenibee <ksj3452@naver.com>
2021-03-22 21:28:53 +0900

    :construction:Impl basic Entity class only
    Impl basic Tag, Note, NoteDetail class with relation

---
+++ src/main/java/cenibee/github/rememote/note/Note.java
@@ -0,0 +1,30 @@
+package cenibee.github.rememote.note;
+
+import cenibee.github.rememote.tag.Tag;
+import lombok.*;
+
+import javax.persistence.*;
+import java.util.List;
+import java.util.Set;
+
+@Entity
+@Builder @Getter @Setter
+@NoArgsConstructor @AllArgsConstructor
+@EqualsAndHashCode(of = {"id", "keyword"})
+@ToString(exclude = "id")
+public class Note {
+
+    @Id
+    @GeneratedValue
+    Long id;
+
+    @Column(unique = true)
+    String keyword;
+
+    @OneToMany
+    List<NoteDetail> details;
+
+    @ManyToMany
+    Set<Tag> tags;
+
+}
