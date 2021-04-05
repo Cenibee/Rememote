4a59429d1c9eb4e16b1c39384655c36213658347
Cenibee <30590761+Cenibee@users.noreply.github.com>
2021-04-05 15:49:03 +0900

    :sparkles: Impl select note
    Impl select note and Update BaseModelAssembler

--- src/main/java/cenibee/github/rememote/common/BaseModelAssembler.java
+++ src/main/java/cenibee/github/rememote/common/BaseModelAssembler.java
@@ -1,11 +1,15 @@
 package cenibee.github.rememote.common;
 
+import org.springframework.hateoas.CollectionModel;
 import org.springframework.hateoas.Link;
 import org.springframework.hateoas.RepresentationModel;
 import org.springframework.hateoas.server.RepresentationModelAssembler;
 import org.springframework.lang.NonNull;
 
 import java.util.Collection;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.StreamSupport;
 
 /**
  * Base assembler class for convert Domain type to Model type.
@@ -14,15 +18,31 @@ import java.util.Collection;
  * @param <T> Origin Domain Type.
  * @param <D> Model Type to Convert
  */
-public abstract class BaseModelAssembler<T, D extends RepresentationModel<D>>
-        implements RepresentationModelAssembler<T, D> {
+public interface BaseModelAssembler<T, D extends RepresentationModel<D>>
+        extends RepresentationModelAssembler<T, D> {
 
     @NonNull
-    public D toModel(@NonNull T note) {
+    @Override
+    default D toModel(@NonNull T note) {
         return assemble(note)
                 .add(links(note));
     }
 
-    abstract protected D assemble(T entity);
-    abstract protected Collection<Link> links(T entity);
+    @NonNull
+    @Override
+    default CollectionModel<D> toCollectionModel(Iterable<? extends T> entities) {
+
+        return StreamSupport.stream(entities.spliterator(), false)
+                .map(this::toModel)
+                .collect(Collectors.collectingAndThen(Collectors.toList(), CollectionModel::of))
+                        .add(links(entities));
+    }
+
+    D assemble(T entity);
+    default Collection<Link> links(T entity) {
+        return List.of();
+    }
+    default Collection<Link> links(Iterable<? extends T> entities) {
+        return List.of();
+    }
 }
