8a079f0615ac641978f1262dfe7fc54b4f5eb563
Cenibee <30590761+Cenibee@users.noreply.github.com>
2021-04-07 15:01:49 +0900

    :construction: Keep page need modify Back-end

--- my-front/src/App.js
+++ my-front/src/App.js
@@ -1,5 +1,7 @@
 import logo from './logo.svg';
 import './App.css';
+import React from 'react'
+import axios from 'axios'
 
 function App() {
   return (
@@ -15,11 +17,56 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn React!!
         </a>
+        <TestList/>
       </header>
     </div>
   );
+}
+
+class TestList extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            notes: [
+                'hello', 'world!'
+            ]
+        }
+    }
+
+    componentDidMount() {
+        axios.get("http://localhost:8080/api/notes")
+            .then(response => {
+                console.log(response.data['_embedded']['noteModelList']);
+                this.setState({
+                    notes: response.data['_embedded']['noteModelList']
+                });
+            })
+            .catch(reason => console.log(reason));
+    }
+
+    render() {
+        let list = this.state.notes.map(note => {
+            let detail = note.details ? note.details.map(detail => <li key={detail.detail}>{detail.detail}</li>) : undefined;
+            let tag = note.tags ? note.tags.map(tag => <li key={tag.name}>{tag.name}</li>) : undefined;
+            return (
+                    <li key={note.keyword}>
+                        {note.keyword}
+                        <ul>
+                            {detail}
+                            {tag}
+                        </ul>
+                    </li>
+                )
+            }
+        )
+
+        return (
+            <ul>{list}</ul>
+        )
+    }
+
 }
 
 export default App;
